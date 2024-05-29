2a87574c2e9507d18f3b3009280f16cb626db312
milcho0604 <milcho0604@gmail.com>
2024-05-29 15:09:04 +0900

    add와 commit 동시에

---
+++ basic_synctax/basic_command.sh
@@ -0,0 +1,20 @@
+# 현재 working directory, staging area 만 상태 확인
+git status
+
+# git add : 모든 수정/추가사항 add
+git add .
+
+# git add 특정파일: 특정 파일 add
+git add 특정파일(위치포함)
+
+# local repository에 반영 및 커밋이력 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
+# vi모드에서 작성 : git commit 후 엔터
+
+# add와 commit 동시에
+git cimmot -am "add와 commit 
+
+
+# commit 이력 확인(local repository의 이력)
+git log
