aa23679e6b43fa7ccaf0c9e640ce0ee4a943513a
milcho0606 <test@naver.com>
2024-05-29 14:10:56 +0900

    fix

--- basic_synctax/env_setting.sh
+++ basic_synctax/env_setting.sh
@@ -11,10 +11,11 @@ git push origin 브랜치명
 
 # github 인증방법 2가지
 # 1. token인증방식 : github에서 직접 보안키(token) 발급
-
+키체인 들어가서 기존에 있던 키를 삭제하고 새로운 키체인을 등록
+이름: https://github.com
+계정: milcho0604
+암호: 토큰
 # 2. 제3자인증(oauth)방식
-
-
 
 
 # git 프로젝트 생성방법 2가지
@@ -34,3 +35,25 @@ git remote set-url origin 레포주소
 
 # git 설정정보 조회
 git config --list
+
+# 타인레포 clone 방법 2가지
+# 1. 커밋이력 그대로 가져오기
+git clone 타인레포주소
+# 해당폴더로 아동 후 git 명령어
+git remote set-url origin 만든레포
+git push origin master # 별도의 add, commit 필요 없고 바로 push 가능
+# 2. 커밋이력 없이 가져오기
+git clone 레포
+# 해당폴더로 이동 후 .git폴더 삭제 (숨긴 파일 보는 방법 = cmd+shift+.)
+git init
+git remote add origin 만든레포
+git add .
+git commit -m "메시지"
+git push origin master
+
+# 사용자 지정 방법
+# 전역적 사용자(이름, email) 지정
+git config --global user.name "milcho0604"
+git config --global user.email "이메일"
+
+# 지역적 사용자(이름, email) 지정
